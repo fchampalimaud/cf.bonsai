6a07f57e95442101ead5d7dccd63a866ab15c062
Luís Teixeira <micboucinha@gmail.com>
2024-06-25 16:58:03 +0100

    update DocFx configuration and build script

--- docs/build.ps1
+++ docs/build.ps1
@@ -1,7 +1,7 @@
 $baseFolder = @("..\")
 
 Write-Host ("Generate SVGs for all examples found..." )
-.\docfx-tools\modules\Export-Image.ps1 -libPath $baseFolder -bootstrapperPath "..\.bonsai\Bonsai.exe" -workflowPath "..\workflows"
+.\docfx-tools\modules\Export-Image.ps1 -libPath $baseFolder -bootstrapperPath "..\.bonsai\Bonsai.exe" -workflowPath "workflows"
 
 # Build documentation
 dotnet docfx @args
