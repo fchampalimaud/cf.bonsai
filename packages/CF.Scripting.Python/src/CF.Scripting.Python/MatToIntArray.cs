f6697a29c6d67ceee7be579f4797fd2d8826d7ca
Hugo Gravato Marques <hgmarques@gmail.com>
2024-06-26 15:35:44 +0100

    Added functions in the python scripting package to convert data into float arrays.

---
+++ packages/CF.Scripting.Python/src/CF.Scripting.Python/MatToIntArray.cs
@@ -0,0 +1,47 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Xml.Serialization;
+using Python.Runtime;
+using OpenCV.Net;
+
+namespace CF.Scripting.Python
+{
+    /// <summary>
+    /// Converts a python object into an integer value <see cref="PyObject"/>
+    /// </summary>
+    [Combinator]
+    [Description("")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class MatToIntArray
+    {
+        public IObservable<int[,]> Process(IObservable<Mat> source)
+        {
+            return source.Select(value => 
+            {
+                return this.ConvertMatToIntArray(value);               
+            });
+        }
+
+        private int[,] ConvertMatToIntArray(Mat value)
+        {
+            int[,] output = new int[value.Rows, value.Cols];
+
+            for (int i = 0; i < value.Rows; i++)
+            {
+                for (int j = 0; j < value.Cols; j++)
+                {
+                    output[i, j] = (int)value[i, j].Val0;
+                }
+            }
+            return output;
+        }
+
+
+    }
+
+
+
+}
