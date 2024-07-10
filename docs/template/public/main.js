6ba706ce1245fecdf83ff9cc5b481fe559fa8d03
José Grilo <jose.grilo@research.fchampalimaud.org>
2024-07-10 13:28:39 +0100

    logo switching based on light/dark mode

--- docs/template/public/main.js
+++ docs/template/public/main.js
@@ -5,3 +5,16 @@ export default {
         WorkflowContainer.init();
     }
 }
+
+const observer = new MutationObserver(() => {
+    if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+      document.getElementById("logo").src = "/images/logo_white.svg"
+    } else {
+      document.getElementById("logo").src = "/images/logo.svg"
+    }
+  });
+  
+  observer.observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ['data-bs-theme'] // only observe changes to this specific attribute
+  });
