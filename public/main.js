2a0d1f80cfc90384604904174e7114f287da87fa
ZegCricket <ZegCricket@users.noreply.github.com>
2025-01-17 11:29:53 +0000

    deploy: f192eb84d3e038be1b4d76d23d1cc44fd4e8bb1a

---
+++ public/main.js
@@ -0,0 +1,20 @@
+import WorkflowContainer from "./workflow.js"
+
+export default {
+    start: () => {
+        WorkflowContainer.init();
+    }
+}
+
+const observer = new MutationObserver(() => {
+    if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+      document.getElementById("logo").src = "https://fchampalimaud.github.io/cf.bonsai/images/logo_white.svg"
+    } else {
+      document.getElementById("logo").src = "https://fchampalimaud.github.io/cf.bonsai/images/logo.svg"
+    }
+  });
+  
+  observer.observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ['data-bs-theme'] // only observe changes to this specific attribute
+  });
